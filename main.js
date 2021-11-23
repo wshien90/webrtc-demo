1fa32c6e8d28ef7617236c6e7c2d8bc17af9634e
Hien Phan <hienp@kpu.ac.kr>
2021-11-24 02:46:24 +0900

    open camera

---
+++ main.js
@@ -0,0 +1,11 @@
+function openStream() {
+    const config = {audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+}
+openStream()
+.then(stream => playStream('localStream', stream))
